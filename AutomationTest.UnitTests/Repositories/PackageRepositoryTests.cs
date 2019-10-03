5a9a53ba9be3c719aec30e3a3554cd5b546ce006
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2019-10-03 12:57:07 +0200

    Add unit tests for package repository

--- AutomationTest.UnitTests/Repositories/PackageRepositoryTests.cs
+++ AutomationTest.UnitTests/Repositories/PackageRepositoryTests.cs
@@ -1,10 +1,13 @@
 ﻿using AutomationTest.Data.Infrastructure;
 using AutomationTest.Data.Models;
+using AutomationTest.Data.Resources;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using MvvmCross.Tests;
 using System;
+using System.Linq;
 using Xunit;
+using static AutomationTest.UnitTests.Repositories.PackageRepositoryTestsData;
 
 namespace AutomationTest.UnitTests.Repositories
 {
@@ -24,7 +27,6 @@ namespace AutomationTest.UnitTests.Repositories
             _repository = Ioc.IoCConstruct<PackageRepositoryDisposable>();
         }
 
-        //[Fact(Skip = "In-memory Realm randomly crashes test runner after assertion")]
         [Fact]
         public void AddPackage_ValidData_PackageAddedToDatabase()
         {
@@ -46,6 +48,94 @@ namespace AutomationTest.UnitTests.Repositories
                 package.IsManaged.Should().BeTrue();
                 package.Realm.Should().NotBeNull();
             }
+        }
+
+        [Fact]
+        public void AddPackage_DuplicatedBarcode_ArgumentExceptionThrown()
+        {
+            // Arrange
+            _repository.AddPackage(new Package
+            {
+                Barcode = "qwerty",
+                Width = 20,
+                Height = 15,
+                Depth = 30
+            });
+
+            var duplicated = new Package
+            {
+                Barcode = "qwerty",
+                Width = 30,
+                Height = 18,
+                Depth = 3.5
+            };
+
+            // Act
+            Action action = () => _repository.AddPackage(duplicated);
+
+            // Assert
+            var expectedMessage = string.Format(ErrorMessages.PackageAlreadyExists, duplicated.Barcode);
+            action.Should().Throw<ArgumentException>().WithMessage(expectedMessage);
+        }
+
+        [Fact]
+        public void GetPackagesForDay_EmptyRepository_EmptyList()
+        {
+            // Act
+            var result = _repository.GetPackagesForRange(TestDate, TestDate);
+
+            // Assert
+            result.Should().BeEmpty();
+        }
+
+        [Theory]
+        [MemberData(nameof(DatesInRange), MemberType = typeof(PackageRepositoryTestsData))]
+        public void GetPackagesForDay_SingleRowInRange_ReturnRow(DateTimeOffset from, DateTimeOffset to, DateTimeOffset date)
+        {
+            // Arrange
+            var entity = new Package
+            {
+                Barcode = "qwerty",
+                Width = 20,
+                Height = 15,
+                Depth = 30,
+                Date = date
+            };
+
+            _repository.AddPackage(entity);
+
+            // Act
+            var result = _repository.GetPackagesForRange(from, to).ToList();
+
+            // Assert
+            using (new AssertionScope())
+            {
+                result.Should().HaveCount(1);
+                entity.Should().BeEquivalentTo(result[0], options => options.ExcludingMissingMembers());
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(DatesOutOfRange), MemberType = typeof(PackageRepositoryTestsData))]
+        public void GetPackagesForDay_SingleRowOutOfRange_EmptyList(DateTimeOffset from, DateTimeOffset to, DateTimeOffset date)
+        {
+            // Arrange
+            var entity = new Package
+            {
+                Barcode = "qwerty",
+                Width = 20,
+                Height = 15,
+                Depth = 30,
+                Date = date
+            };
+
+            _repository.AddPackage(entity);
+
+            // Act
+            var result = _repository.GetPackagesForRange(from, to).ToList();
+
+            // Assert
+            result.Should().HaveCount(0);
         }
 
         public void Dispose()
---
+++ AutomationTest.UnitTests/Repositories/PackageRepositoryTestsData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutomationTest.UnitTests.Repositories
+{
+    public class PackageRepositoryTestsData
+    {
+        public static DateTimeOffset TestDate { get; } = new DateTimeOffset(
+            2019, 10, 03, 10, 30, 15, TimeSpan.Zero);
+
+        public static IEnumerable<object[]> DatesInRange() => new List<object[]>
+        {
+            new object []
+            {
+                TestDate,
+                TestDate.AddHours(2),
+                TestDate.AddHours(1)
+            },
+            new object []
+            {
+                TestDate,
+                TestDate.AddHours(2),
+                TestDate
+            },
+            new object []
+            {
+                TestDate,
+                TestDate.AddHours(2),
+                TestDate.AddHours(2)
+            }
+        };
+
+        public static IEnumerable<object[]> DatesOutOfRange() => new List<object[]>
+        {
+            new object []
+            {
+                TestDate,
+                TestDate.AddHours(2),
+                TestDate.AddHours(3)
+            },
+            new object []
+            {
+                TestDate,
+                TestDate.AddHours(2),
+                TestDate.AddMilliseconds(-1)
+            },
+            new object []
+            {
+                TestDate,
+                TestDate.AddHours(2),
+                TestDate.AddHours(2).AddMilliseconds(1)
+            }
+        };
+    }
+}
