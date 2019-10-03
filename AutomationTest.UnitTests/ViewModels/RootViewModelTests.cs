082f956fe1ef3a1d08639049cc87fc035c455f7d
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2019-10-03 13:18:33 +0200

    Add unit tests for RootViewModel

---
+++ AutomationTest.UnitTests/ViewModels/RootViewModelTests.cs
@@ -0,0 +1,36 @@
+﻿using AutomationTest.Core.ViewModels;
+using MvvmCross.Navigation;
+using MvvmCross.Tests;
+using NSubstitute;
+using Xunit;
+
+namespace AutomationTest.UnitTests.ViewModels
+{
+    public class RootViewModelTests : MvxIoCSupportingTest
+    {
+        public RootViewModelTests()
+        {
+            Setup();
+        }
+
+        protected override void AdditionalSetup()
+        {
+            var navigationService = Substitute.For<IMvxNavigationService>();
+            Ioc.RegisterSingleton(navigationService);
+        }
+
+        [Fact]
+        public void ViewAppeared_MethodInvoked_NavigatedToMenuViewModel()
+        {
+            // Arrange
+            var navigationService = Ioc.Resolve<IMvxNavigationService>();
+            var viewModel = Ioc.IoCConstruct<RootViewModel>();
+
+            // Act
+            viewModel.ViewAppeared();
+
+            // Assert
+            navigationService.Received(1).Navigate<MenuViewModel>();
+        }
+    }
+}
